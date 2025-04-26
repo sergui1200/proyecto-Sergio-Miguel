4f95043fc4a9a30e77a599e837f91fdae6cc19e5
Usuario <CORREO>
2025-04-26 11:26:12 +0200

    Instalacón SQL

--- src/instalacion.sql
+++ src/instalacion.sql
@@ -9,5 +9,6 @@ aprobado BOOLEAN
 
 INSERT INTO dam1 VALUES (1,'Sergio','Buendia Colao','PRO ED INGLES',5,TRUE)
 ,(2,'Miguel','Platon cano','PRO ED INGLES',4,FALSE),
-(3,'Pablo','Paraiso','PRO BD XML FOL ED INGLES',7,TRUE)
-,(4,'Jorge','CASAS','PRO BD XML FOL ED INGLES',9,TRUE),(5,'Jorge','Sancho','PRO ED INGLES',6,TRUE);
+(3,'Pablo','Paraiso','PRO BD XML FOL ED INGLES',7,TRUE),
+(4,'Jorge','CASAS','PRO BD XML FOL ED INGLES',9,TRUE),
+(5,'Jorge','Sancho','PRO ED INGLES',6,TRUE);
